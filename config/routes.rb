4d30d85d2042d42baf33e6f0fc727aab021d648c
rbarnat <rbarnat@gmail.com>
2020-11-19 19:43:23 +0400

    emails controlers et routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "home#index"
+  # root to: "home#index"
   resources :tasks, except: [:show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "emails#index"
+  resources :emails, only: [:index, :show, :create]
 end
